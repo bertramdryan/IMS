e2739c0b24107698f9f8b80fd90742ebb104b720
Ryan Bertram <rbertram@cdi.local>
2023-06-16 17:17:51 -0400

    First Commit

---
+++ IMS.UseCases/Inventories/ViewInventoryByNameUseCase.cs
@@ -0,0 +1,20 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Inventories.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories
+{
+    public class ViewInventoryByNameUseCase : IViewInventoryByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoryByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+        }
+    }
+}
